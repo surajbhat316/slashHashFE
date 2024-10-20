7e705be8856643249da9f4f299e0a1b598f8c0f9
Avineet Bhat <surajbhat316@gmail.com>
2024-10-20 11:34:41 +0530

    Added the routes for the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Search from "./Pages/Search/search";
+import Favourites from "./Pages/Favourites/favourites";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <Link to="/" class="navbar-brand" href="#hello">
+              Navbar
+            </Link>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <Link 
+                  to="/"
+                  class="nav-link active"
+                  aria-current="page"
+                >
+                  Search
+                </Link>
+                <Link
+                  to="/favourites" 
+                  class="nav-link">
+                  Favourites
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Search/>}></Route>
+        <Route path="/favourites" element={<Favourites/>}></Route>
+      </Routes>
+    </>
   );
 }
 
